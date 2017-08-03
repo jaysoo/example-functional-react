d0e07b10cb0af1a407a0498d257c426e38d655e5
Jack Hsu <jack.hsu@gmail.com>
2017-08-03 16:14:47 -0400

    Adds functor test for Component.

--- src/react/Component.test.js
+++ src/react/Component.test.js
@@ -54,6 +54,55 @@ test('Contravariant', () => {
   expect(y1.html()).toEqual(y2.html())
 })
 
+test('Functor', () => {
+  const a = Component({
+    view: View(({ state, dispatch }) =>
+      <div>
+        <button onClick={() => dispatch({ type: 'INC' })}>
+          increment
+        </button>
+        <p>
+          {state}
+        </p>
+      </div>
+    ),
+    reducer: Reducer((state, action) => {
+      switch (action.type) {
+        case 'INC':
+          return state + 1
+        default:
+          return state
+      }
+    })
+  })
+
+  // Identity
+  const x1 = shallow(a.map(x => x).startWith(10))
+  const x2 = shallow(a.startWith(10))
+
+  expect(x1.html()).toEqual(x2.html())
+
+  x1.find('button').simulate('click')
+  x2.find('button').simulate('click')
+
+  expect(x1.html()).toEqual(x2.html())
+
+  // Composition
+  const f = x => <div>x: {x}</div>
+  const g = y => <div>y: {y}</div>
+  const compLaw1 = a.map(compose(f, g))
+  const compLaw2 = a.map(g).map(f)
+  const y1 = shallow(compLaw1.startWith(10))
+  const y2 = shallow(compLaw2.startWith(10))
+
+  expect(y1.html()).toEqual(y2.html())
+
+  y1.find('button').simulate('click')
+  y2.find('button').simulate('click')
+
+  expect(y1.html()).toEqual(y2.html())
+})
+
 test('Profunctor', () => {
   const a = Component({
     view: View(({ state, dispatch }) =>
